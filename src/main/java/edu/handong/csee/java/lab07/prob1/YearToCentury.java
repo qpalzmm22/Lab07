b025bb3d620f3e7b24c22c16f257295a292d224c
Isaac Chae <qpalzmm22@gmail.com>
2018-03-30 18:04:46 +0900

    prob1 : methods and constructor

---
+++ src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
@@ -0,0 +1,18 @@
+package edu.handong.csee.java.lab07.prob1;
+
+public class YearToCentury {
+	int year;
+	
+	public YearToCentury(int year) {
+	}
+	
+	public int calcCentury(int year) {
+		return year;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
