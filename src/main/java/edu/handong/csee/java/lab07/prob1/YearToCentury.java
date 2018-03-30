5a62b3aa924da33cecd57f8b66ad057d2536598a
Isaac Chae <qpalzmm22@gmail.com>
2018-03-30 18:12:58 +0900

    Complete code

--- src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
+++ src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
@@ -1,18 +1,31 @@
 package edu.handong.csee.java.lab07.prob1;
 
+import java.util.*;
+
 public class YearToCentury {
 	int year;
+	int century;
 	
 	public YearToCentury(int year) {
+		this.year = year;
 	}
 	
-	public int calcCentury(int year) {
-		return year;
+	public int calcCentury() {
+		century = (year - 1) / 100 + 1; 
+		return century;
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int input;
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Input year: ");
+		input = keyboard.nextInt();
+		
+		YearToCentury calcCent = new YearToCentury(input);
+		
+		System.out.println(input + " is " + calcCent.calcCentury() + "th century.");
 	}
 
 }
