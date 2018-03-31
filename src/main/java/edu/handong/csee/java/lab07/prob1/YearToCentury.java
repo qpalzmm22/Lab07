fbe78b409431ab7d57e41900aa4286f809a6ad22
Isaac Chae <qpalzmm22@gmail.com>
2018-03-31 19:40:03 +0900

    p1 line comments added

--- src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
+++ src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
@@ -1,6 +1,6 @@
-package edu.handong.csee.java.lab07.prob1;
+package edu.handong.csee.java.lab07.prob1;	// makes it a package 
 
-import java.util.*;
+import java.util.*;	// import to use Scanner
 
 /**
  * This class implements YearToCentury.
@@ -9,17 +9,17 @@ import java.util.*;
  * @author qpalzmm22
  *
  */
-public class YearToCentury {
-	int year;
-	int century;
+public class YearToCentury {	// class to implement calculation of year to century
+	int year;		// a variable to hold given year
+	int century;	// a variable to store the century value
 	
 	/**
 	 * This is the constructor of YearToCentury
 	 * @param year
 	 * this constructor takes one parameter of year to save in this class
 	 */
-	public YearToCentury(int year) {
-		this.year = year;
+	public YearToCentury(int year) {	// a constructor that saves given parameter as this.year
+		this.year = year;	// this class' year is assign to given parameter
 	}
 	
 	/**
@@ -27,9 +27,12 @@ public class YearToCentury {
 	 * @return century
 	 * This method returns century after calculation
  	 */
-	public int calcCentury() {
-		century = (year - 1) / 100 + 1; 
-		return century;
+	public int calcCentury() {		// a method to calculate the century
+		century = (year - 1) / 100 + 1; // year-1 to get rid of exceptions like 1900
+										// making them go lower
+										// then divide them by 100 to get the hudreth value
+										// and add one to get actual value of century
+		return century;					// the output of this method
 	}
 	
 	/**
@@ -37,17 +40,19 @@ public class YearToCentury {
 	 * @param args
 	 * no arguments are given for cmd line
 	 */
-	public static void main(String[] args) {
-		int input;
+	public static void main(String[] args) { // a main method to check if constructed class is working
+		int input;			// a variable to store user's input
 		
-		Scanner keyboard = new Scanner(System.in);
+		Scanner keyboard = new Scanner(System.in);	// initialize to use nextInt() 
 		
-		System.out.println("Input year: ");
-		input = keyboard.nextInt();
+		System.out.println("Input year: ");	// ask user for year
+		input = keyboard.nextInt();			// store user's answer in input
 		
-		YearToCentury calcCent = new YearToCentury(input);
+		YearToCentury calcCent = new YearToCentury(input);	// initialize by (input) to receive the parameter of user's input to use calcCentury()
 		
-		System.out.println(input + " is " + calcCent.calcCentury() + "th century.");
+		System.out.println(input + " is " + calcCent.calcCentury() + "th century.");	// print out the result of calcCentury()
+		
+		keyboard.close();	// close keyboard to prevent resource leak
 	}
 
 }
