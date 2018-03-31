338e4a13708896b94d4796b971704188540c54ee
Isaac Chae <qpalzmm22@gmail.com>
2018-03-31 22:00:36 +0900

    compelete code

--- src/main/java/edu/handong/csee/java/lab07/prob3/Attendence.java
+++ src/main/java/edu/handong/csee/java/lab07/prob3/Attendence.java
@@ -1,23 +1,43 @@
 package edu.handong.csee.java.lab07.prob3;
 
+import java.util.Random;
+
 public class Attendence {
 	String name;
 	int grade;
 	int studentNum;
+	int absent;
 	
 	public Attendence() {
+		setName("");
+		setGrade(1);
+		setStudentNum(00000000);
+		assignAbsent();
 	}
 	
 	public Attendence(String name, int grade, int stdnum) {
+		setName(name);
+		setGrade(grade);
+		setStudentNum(stdnum);
+		assignAbsent();
 	}
 	
-	public void setName() {
+	public void setName(String name) {
+		this.name = name;
 	}
 	
-	public void setGrade() {
+	public void setGrade(int grade) {
+		this.grade = grade;
 	}
 	
-	public void setStudentNum() {
+	public void setStudentNum(int stdnum) {
+		this.studentNum = stdnum;
+	}
+	
+	public void assignAbsent() {
+		Random randomGen = new Random();
+		
+		this.absent = randomGen.nextInt(10);
 	}
 	
 	public String getName() {
@@ -32,11 +52,36 @@ public class Attendence {
 		return this.studentNum;
 	}
 	
-	public void assignAbsent() {
+	public int getAbsent() {
+		return this.absent;
+	}
+	
+	public void checkFail() {
+		if(this.absent > 6) 
+			System.out.println("I'm sorry, " + getName() + ". You failed this course" );
+		else
+			System.out.println("Congrats, "+ getName() + " You passed the class");
+		System.out.println("You missed " + getAbsent() + " times");
 	}
 
 	public static void main(String[] args) {
-
+		Attendence student1 = new Attendence();
+		Attendence student2 = new Attendence("Kent", 2, 21700111);
+		Attendence student3 = new Attendence("Lucas", 1, 21833222);
+		Attendence student4 = new Attendence("Martha", 2, 21733444);
+		
+		student1.setName("Jess");
+		student1.setGrade(4);
+		student1.setStudentNum(21400999);
+		
+		System.out.println("\nChecking Student 1");
+		student1.checkFail();
+		System.out.println("\nChecking Student 2");
+		student2.checkFail();
+		System.out.println("\nChecking Student 3");
+		student3.checkFail();
+		System.out.println("\nChecking Student 4");
+		student4.checkFail();
 	}
 
 }
