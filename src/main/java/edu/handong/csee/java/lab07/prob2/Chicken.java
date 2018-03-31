529057496ffa9d8c5112e7fa98592767f1c19a91
Isaac Chae <qpalzmm22@gmail.com>
2018-03-31 19:48:42 +0900

    p2 methods and constructor outline

---
+++ src/main/java/edu/handong/csee/java/lab07/prob2/Chicken.java
@@ -0,0 +1,37 @@
+package edu.handong.csee.java.lab07.prob2;
+
+public class Chicken {
+	String name;
+	int price;
+	int starNum;
+	
+	public Chicken(String name, int price, int starNum) {
+	}
+	
+	public void setName() {
+	}
+	
+	public void setPrice() {
+	}
+	
+	public void setStarNum() {
+	}
+	
+	public String getname() {
+		return name;
+	}
+	
+	public int price() {
+		return price;
+	}
+	
+	public int starNum() {
+		return starNum;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
