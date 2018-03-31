c7a26eaed553e4c00050d812ed5ce9e40dda6509
Isaac Chae <qpalzmm22@gmail.com>
2018-03-31 21:40:17 +0900

    p3 Constructors and method outline

---
+++ src/main/java/edu/handong/csee/java/lab07/prob3/Attendence.java
@@ -0,0 +1,42 @@
+package edu.handong.csee.java.lab07.prob3;
+
+public class Attendence {
+	String name;
+	int grade;
+	int studentNum;
+	
+	public Attendence() {
+	}
+	
+	public Attendence(String name, int grade, int stdnum) {
+	}
+	
+	public void setName() {
+	}
+	
+	public void setGrade() {
+	}
+	
+	public void setStudentNum() {
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getGrade() {
+		return this.grade;
+	}
+	
+	public int getStudentNum() {
+		return this.studentNum;
+	}
+	
+	public void assignAbsent() {
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
